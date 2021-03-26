0c26ac5d06b0110efce738f8428a5d3c5a1f765c
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-03-26 20:23:39 +0600

    initial states complete

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1Lym0t5TUti71EoGRw_r9AOv6D4crjiA",
+    authDomain: "burj-al-arab-31219.firebaseapp.com",
+    projectId: "burj-al-arab-31219",
+    storageBucket: "burj-al-arab-31219.appspot.com",
+    messagingSenderId: "905030971072",
+    appId: "1:905030971072:web:b2aa2856382840eaf90725"
+};
+export default firebaseConfig;
